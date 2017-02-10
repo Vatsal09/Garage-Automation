6638b30c87f00ca8ea06fae878e5092fe2c4aed9
Vatsal09 <vpan17@gmail.com>
2017-02-09 21:36:34 -0500

    Changed absolute path to relative path for the image sourcing, allowing for portability

--- License Plate Reader/cloudapi/python/test.py
+++ License Plate Reader/cloudapi/python/test.py
@@ -5,8 +5,8 @@ apiclient = openalpr_api.DefaultApi()
 key = os.environ.get('OPENALPR_SECRET_KEY', "sk_DEMODEMODEMODEMODEMODEMO")
 print("Using key: %s" % key)
 
-response = apiclient.recognize_post(key, "plate,color,make,makemodel", image="C:\Users\Panda\Documents\GitHub\Garage-Automation\License Plate Reader\openalpr\samples\us-3.jpg", country="us")
-
+#response = apiclient.recognize_post(key, "plate,color,make,makemodel", image="C:\Users\Panda\Documents\GitHub\Garage-Automation\License Plate Reader\openalpr\samples\us-3.jpg", country="us")
+response = apiclient.recognize_post(key, "plate,color,make,makemodel", image=".\..\..\Images\us-1.jpg", country="us")
 
 # print dir(response.plate.to_dict())
 # plate = response.plate.to_dict()
@@ -17,7 +17,7 @@ response = apiclient.recognize_post(key, "plate,color,make,makemodel", image="C:
 # 	print "Plate:      %s - %f percent" % (res.plate, res.confidence)
 
 
-# print "Plate:      %s - %f percent" % (response.plate.results[0].plate, response.plate.results[0].confidence)
+#print "Plate:      %s - %f percent" % (response.plate.results[0].plate, response.plate.results[0].confidence)
 # print "Color:      %s - %f percent" % (response.color[0].value, response.color[0].confidence)
 # print "Make:       %s - %f percent" % (response.make[0].value, response.make[0].confidence)
 # print "Make-model: %s - %f percent" % (response.makemodel[0].value, response.makemodel[0].confidence)
