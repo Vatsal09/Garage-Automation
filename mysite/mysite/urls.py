5466a0c165fdd8e00a69809af8679f7e452934a7
Gao Pan <gaopan1996@gmail.com>
2017-02-18 17:33:02 -0500

    Added Django-leaflet and Django-geojson to INSTALLED APPS in settings Updated required imported packages in urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
-from django.contrib import admin
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.views.generic import TemplateView
+from djgeojson.views import GeoJSONLayerView
 urlpatterns = [
     url(r'^parking/', include('parking.urls')),
     url(r'^admin/', admin.site.urls),
