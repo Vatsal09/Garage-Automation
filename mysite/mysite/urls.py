1da711df356ba8e571b983da979611c6b682525a
Gao Pan <gaopan1996@gmail.com>
2017-02-18 17:05:44 -0500

    Change 'Parking' to 'parking' to reflect app name change

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 urlpatterns = [
-    url(r'^Parking/', include('Parking.urls')),
+    url(r'^parking/', include('parking.urls')),
     url(r'^admin/', admin.site.urls),
 ]
