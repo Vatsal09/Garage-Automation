76b416a24ec100644a23816231afd818f3ca9519
Vatsal09 <vpan17@gmail.com>
2017-03-15 23:46:30 -0400

    venv update

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\panda\documents\github\garage-automation\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
