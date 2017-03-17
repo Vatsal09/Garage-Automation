ad2571ab1ea90834346c4c8ab03aa365255597e4
Vatsal09 <vpan17@gmail.com>
2017-03-16 22:04:36 -0400

    Successful model build, resolved runserver error, and completed migrations

--- testProject/parking/views.py
+++ testProject/parking/views.py
@@ -14,25 +14,25 @@ def index(request):
 	return render(request, 'parking/login.html')
 
 def add_lot(request):
-	return HttpResponse("<h1> add_lot </h1>") 
+	HttpResponse("<h1> add_lot </h1>") 
 
 def delete_lot(request):
-	return HttpResponse("<h1> delete_lot </h1>") 
+	HttpResponse("<h1> delete_lot </h1>") 
 
 def detail(request):
-	return HttpResponse("<h1> detail </h1>") 
+	HttpResponse("<h1> detail </h1>") 
 
 def add_spot(request):
-	return HttpResponse("<h1> add_spot </h1>") 
+	HttpResponse("<h1> add_spot </h1>") 
 
 def delete_spot(request):
-	return HttpResponse("<h1> delete_spot </h1>") 
+	HttpResponse("<h1> delete_spot </h1>") 
 
 def register_manager(request):
-	return HttpResponse("<h1> register manager </h1>")
+	HttpResponse("<h1> register manager </h1>")
 
 def login_manager(request):
-	return HttpResponse("<h1> login manager </h1>")
+	HttpResponse("<h1> login manager </h1>")
 
 def logout_manager(request):
-	return HttpResponse("<h1> logout manager </h1>")
+	HttpResponse("<h1> logout manager </h1>")
