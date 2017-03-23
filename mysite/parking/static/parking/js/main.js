eb33ddd85a059b6d5108436747fb72cb22c52a28
Vatsal09 <vpan17@gmail.com>
2017-03-22 22:43:52 -0400

    Gantt charts added and tried to do dynamic handling of main tmeplate images

--- mysite/parking/static/parking/js/main.js
+++ mysite/parking/static/parking/js/main.js
@@ -1,3 +1,11 @@
+var path = 'http://www.lucashanyok.com/images_1/',
+    imgs = ['carrot.gif','neil_1.jpg','steak.jpg'],
+    i = Math.floor(Math.random()*imgs.length);
+$('.el').append("<img src='"+path+imgs[i]+"'>");
+
+
+
+
 
 var AlbumsListPage = {
 	init: function() {
