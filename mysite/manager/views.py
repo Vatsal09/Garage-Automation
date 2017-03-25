3a2039fc62d26edfe78fad7f8b1ab9734f3e7b42
Gao Pan <gaopan1996@gmail.com>
2017-03-25 19:28:09 -0400

    Fixed an issue created with merging with Yunqi branch

--- mysite/manager/views.py
+++ mysite/manager/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 
 # Create your views here.
-=======
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
 	return HttpResponse("This is the manager's page.")
->>>>>>> Yunqi
