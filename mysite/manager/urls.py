f3a305a0c572e6fc041662da8ac07cff2bfaf268
Yunqi Shen <syqrefind@gmail.com>
2017-03-27 02:13:13 -0400

    Added sampledata for presenting manager's page

--- mysite/manager/urls.py
+++ mysite/manager/urls.py
@@ -1,11 +1,17 @@
+
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
+#from . import views
+#import sys
+#sys.setrecursionlimit(10000)
 app_name="manager"
 # /manager/
 urlpatterns =[
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name='index'), #index page
+	
+	
 #	url(r'^main/$', views.main, name='main'),
 
 
