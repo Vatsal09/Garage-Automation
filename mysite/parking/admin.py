bb8cc11bd4056ea18abc6bdc17478ca2140b1138
Gao Pan <gaopan1996@gmail.com>
2017-04-23 12:27:59 -0700

    Added ActiveSessions to admin menu

--- mysite/parking/admin.py
+++ mysite/parking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Parking_Lot, Spot, Session
+from .models import Parking_Lot, Spot, Session, ActiveSession
 from garageAutomation.models import Account, Vehicle, PaymentMethod
 
 # Register your models here.
@@ -9,3 +9,4 @@ admin.site.register(Session)
 admin.site.register(Account)
 admin.site.register(Vehicle)
 admin.site.register(PaymentMethod)
+admin.site.register(ActiveSession)
